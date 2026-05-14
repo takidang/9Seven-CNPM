5af459f9959091eac5db4817c012c4dbee2417d6
takidang <taidang87555@gmail.com>
2026-05-14 13:58:30 +0700

    Add project files.

---
+++ 9Seven-CNPM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _9Seven_CNPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
